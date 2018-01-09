2c7d1037c337ce0f7f1dc60b4048ddc5d6a1c899
mlnorby <35051667+mlnorby@users.noreply.github.com>
2018-01-08 19:29:38 -0800

    Update HX711_Test_Sketch.ino

--- HX711_Test_Sketch.ino
+++ HX711_Test_Sketch.ino
@@ -2,33 +2,21 @@
 #include "HX711.h"
 
 #define RST 4
-#define DOUT  3
-#define CLK  2
+#define DOUT  3					//Pin #A1
+#define CLK  2					//Pin #A0
 
-// Create our LoadCell object
-HX711 scale(DOUT, CLK);
+HX711 scale(DOUT, CLK);				//create LoadCell object
 
-// --------------------------------------------------------------------------------------
-// SET UP	SET UP   SET UP   SET UP   SET UP   SET UP   SET UP   SET UP   SET UP
-// --------------------------------------------------------------------------------------
 void setup() {
-	Serial.begin(9600);
-
+	Serial.begin(9600);			//38400
 	scale.set_scale();
-	scale.tare();  //Reset the scale to 0
+	scale.tare();  				//Reset the scale to 0
 }
 
-// --------------------------------------------------------------------------------------
-// LOOP    LOOP    LOOP    LOOP    LOOP    LOOP    LOOP    LOOP    LOOP    LOOP     LOOP
-// --------------------------------------------------------------------------------------
 void loop() {
-	//Adjust to this calibration factor
-	scale.set_scale(360.3);
-
-	// Read an average of X readings
-	Serial.println("Reading weight");
+	scale.set_scale(360.3);			//Adjust to this calibration factor
+	Serial.println("Reading weight");	//Read an average of X readings
 	float weight = scale.get_units(10);
-
 	Serial.print(weight);
 	Serial.println(" g");
 }
